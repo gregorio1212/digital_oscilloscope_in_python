a46697348a2d87ee28ee34bbdbf951bc2202db59
Gregório Luz <30591064+gregorio1212@users.noreply.github.com>
2021-01-24 20:48:13 +0100

    Update digital_oscilloscope.py

--- digital_oscilloscope.py
+++ digital_oscilloscope.py
@@ -4,41 +4,50 @@
  # @brief digital_oscilloscope plotting ADC values from .txt file
 from matplotlib import pyplot as plt
 from matplotlib.widgets import Cursor
-#5 V - 10 bits ADC 5/1023=0.0048
+import numpy as np
+import scipy.fftpack
+
+
 ADC_file = open("ADC_data.txt","r")
 freq_s = 900
 delta_t = 1/freq_s
 ADC_data_ar = []
-time_ar = []
-time = 0
-num_samples = 0
+num_samples = 500
+time_ar = np.linspace(0.0, num_samples*delta_t, num_samples)
+#5 V - 10 bits ADC 5/1023=0.0048
 bit_multiplier = 0.0048
 
-#filling the time array and the ADC array
-while num_samples < 100:
-    temp = ADC_file.readline()
-    time_ar.append(time)
-    time += delta_t
-    temp = int(temp,16)*bit_multiplier
+#filling the ADC array
+for i in time_ar:
+    temp = int(ADC_file.readline(),16)*bit_multiplier
     ADC_data_ar.append(temp)
-    num_samples +=1
-
-#plotting
-fig = plt.figure()
+#plot 1
+fig = plt.figure(1)
 ax = fig.subplots()
 ax.plot(time_ar,ADC_data_ar,color='blue')
+plt.xlabel("time [s]")
+plt.ylabel("Voltage [V]")
+plt.title('digital_oscilloscope')
 ax.grid()
 
-#defining the cursor
+#plot 2 FFT
+#xf = np.linspace(0.0, 1.0/(2.0*delta_t), num_samples/2)
+#yf = scipy.fftpack.fft(ADC_data_ar)
+#fig2 = plt.figure()
+#ax2 = fig2.subplots()
+#ax2.plot(xf,2.0/num_samples*np.abs(yf[0:num_samples/2],color='red')
+#ax2.grid()
+
+#defining the cursor for plot 1
 cursor = Cursor(ax, horizOn = True, vertOn=True, color='red', linewidth=1, useblit=True)
 
-#Creating the annotation framework
+#Creating the annotation framework for plot 1
 annot = ax.annotate("", xy=(0,0), xytext=(-40,40),textcoords="offset points", bbox=dict(boxstyle="round4", fc="grey", ec="k", lw=2), arrowprops=dict(arrowstyle="-|>"))
 annot.set_visible(False)
 
-#Function for storing and showing the clicked values
+#Function for storing and showing the clicked values FOR PLOT 1
 coord = []
-#variable to clean coord after two points
+#variable to clean coord after two points FOR PLOT 1
 control = 0
 def onclick(event):
     global coord
@@ -65,8 +74,4 @@ def onclick(event):
 
 fig.canvas.mpl_connect('button_press_event', onclick)
 
-plt.xlabel("time [s]")
-plt.ylabel("Voltage [V]")
-plt.title('digital_oscilloscope')
-plt.legend()
 plt.show()
